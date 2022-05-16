7a4aa6a65663df554166aac3ae77ac568db8cd93
HEGHELON <humphreyeghelonohor@yahoo.com>
2022-05-16 19:38:13 +0100

    modified index.html, style.css & script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log(5)
+function pictureGenerator() {
+  let image = document.createElement("img");
+  let div = document.getElementById("generatedPictures");
+  let id = Math.floor(Math.random() * 1000 + 1);
+  image.src = `https://picsum.photos/id/${id}/200`;
+  div.appendChild(image);
+  console.log(id);
+}
